7143144dcff1831a9706f678799920be100e3380
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-08-17 20:46:11 -0600

    gitignore, vite, build with React TS + SWC

---
+++ setup_script.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+source ~/.bashrc
+
+# install NodeJS
+nvm use node
+
+# install pnpm
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+
