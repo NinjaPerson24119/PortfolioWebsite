c9011a4afabc627a8b2de3c0f2ed9e17291015c5
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-08-21 22:20:27 -0600

    condition vite config on env

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === 'serve') {
+    return {
+      // dev specific config
+      plugins: [react()],
+      build: {
+        sourcemap: true,
+      }
+    }
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+      plugins: [react()],
+    }
+  }
+})
