166db02d8c8bc23270043cc068180331fd4bfb29
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-09-06 17:01:53 -0600

    add media URLs to gdrive

--- src/constants.tsx
+++ src/constants.tsx
@@ -5,3 +5,10 @@ export const SOCIAL_URLS = {
   GITHUB: 'https://github.com/NinjaPerson24119',
   LINKEDIN: 'https://www.linkedin.com/in/nwengel',
 };
+
+export const MEDIA_URLS = {
+  ARVP_2019_SOFTWARE_TEAM_ONBOARDING:
+    'https://drive.google.com/file/d/17Q1AsXiFBKt4f7ZhbChZ5widCh8Eg2Az/view?usp=sharing',
+  ARVP_COMPUTER_VISION_PRESENTATION:
+    'https://docs.google.com/presentation/d/1kmMbMbsBQIfZQ7aow6z44eJ3_zzTQ7BT/edit?usp=sharing&ouid=116338530486484625609&rtpof=true&sd=true',
+};
