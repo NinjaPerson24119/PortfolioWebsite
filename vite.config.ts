b77a0840cab280c40b0ffc6d728bff575de71634
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-09-11 14:21:55 -0600

    add frontmatter and supporting plugin to mdx

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import mdx from '@mdx-js/rollup';
 import react from '@vitejs/plugin-react-swc';
+import remarkFrontmatter from 'remark-frontmatter';
 import { UserConfig, defineConfig } from 'vite';
 
 function configGenerator(dev: boolean): UserConfig {
   return {
-    plugins: [mdx({}), react()],
+    plugins: [
+      mdx({
+        remarkPlugins: [remarkFrontmatter],
+      }),
+      react(),
+    ],
     build: {
       sourcemap: dev,
     },
