03d63ca23da2dd27e47526e6c60cf6a3929c29f6
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-09-13 00:34:16 -0600

    fix translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,17 +1,17 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import englishTranslation from './assets/locales/en.json';
-import frenchTranslation from './assets/locales/fr.json';
+import translationEn from './assets/locales/en.json';
+import translationFr from './assets/locales/fr.json';
 
 void i18n.use(initReactI18next).init({
+  fallbackLng: 'en',
   lng: 'en',
   resources: {
     en: {
-      englishTranslation,
+      translation: translationEn,
     },
     fr: {
-      // eslint-disable-next-line
-      frenchTranslation,
+      translation: translationFr,
     },
   },
 });
