69b710e0f8ecfd5eec50a3cd9b9c5a9e85fd9908
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-09-16 00:14:00 -0600

    fix: language switcher, and delete a bunch of code :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,26 @@
-import { useState, useMemo } from 'react';
+import { useMemo } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router-dom';
 import { BackgroundEllipse } from './background-ellipse/BackgroundEllipse';
 import { Footer } from './footer/Footer';
-import {
-  Language,
-  LanguageContext,
-} from './language-switcher/language-context';
 import { Layout } from './layout/Layout';
 import { Navigation } from './navigation/Navigation';
 import { GenerateRouter, LayoutProps } from './routing';
 import { Theme } from './theme/Theme';
 
 export function App() {
-  const [language, setLanguage] = useState<Language>('english');
-  const languageContext = useMemo(
-    () => ({
-      language: language,
-      setLanguage: setLanguage,
-    }),
-    [language],
-  );
-
   const layout = ({ content }: LayoutProps) => (
     <Layout navigation={<Navigation />} footer={<Footer />} content={content} />
   );
   const router = useMemo(() => GenerateRouter(layout), [layout]);
 
   return (
-    <LanguageContext.Provider value={languageContext}>
-      <HelmetProvider>
-        <Theme>
-          <RouterProvider router={router} />
-          <BackgroundEllipse diameter="250px" top="-100px" left="-100px" />
-          <BackgroundEllipse diameter="200px" bottom="-50px" right="-50px" />
-        </Theme>
-      </HelmetProvider>
-    </LanguageContext.Provider>
+    <HelmetProvider>
+      <Theme>
+        <RouterProvider router={router} />
+        <BackgroundEllipse diameter="250px" top="-100px" left="-100px" />
+        <BackgroundEllipse diameter="200px" bottom="-50px" right="-50px" />
+      </Theme>
+    </HelmetProvider>
   );
 }
--- src/language-switcher/LanguageSwitcher.tsx
+++ src/language-switcher/LanguageSwitcher.tsx
@@ -1,18 +1,19 @@
 import { Switch, Box, Typography, useTheme } from '@mui/material';
-import { useContext } from 'react';
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { LanguageContext, LanguageContextProps } from './language-context';
 
 export function LanguageSwitcher() {
   const theme = useTheme();
   const { t, i18n } = useTranslation();
-  const languageContext = useContext<LanguageContextProps>(LanguageContext);
+  const languageSwitchOn = useMemo(() => {
+    return i18n.language === 'en';
+  }, [i18n.language]);
   const switchLanguage = () => {
-    if (languageContext.language === 'english') {
-      languageContext.setLanguage('french');
+    if (i18n.language === 'en') {
+      //languageContext.setLanguage('french');
       void i18n.changeLanguage('fr');
     } else {
-      languageContext.setLanguage('english');
+      //languageContext.setLanguage('english');
       void i18n.changeLanguage('en');
     }
   };
@@ -22,7 +23,7 @@ export function LanguageSwitcher() {
         {t('LANGUAGES.FRENCH')}
       </Typography>
       <Switch
-        checked={languageContext.language === 'english'}
+        checked={languageSwitchOn}
         onClick={switchLanguage}
         color="secondary"
       ></Switch>
