e3b9bbb37b4f1feaf454a76f5729ec8eaf344a23
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-09-16 14:13:43 -0600

    restyle loading

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -1,5 +1,21 @@
-import { CircularProgress } from '@mui/material';
+import { CircularProgress, useTheme, Box } from '@mui/material';
 
 export function Loading() {
-  return <CircularProgress />;
+  const theme = useTheme();
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'flex',
+        alignItems: 'flex',
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <CircularProgress
+        sx={{ color: theme.palette.tertiary.main }}
+        size="large"
+      />
+    </Box>
+  );
 }
