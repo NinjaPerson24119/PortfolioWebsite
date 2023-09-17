a1e370abc1d2de95649b57b30c4b240e03ac26c0
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-09-16 18:06:06 -0600

    Revert "rm suspense because page loads really fast anyway"
    This reverts commit bd988e74fc1cda81f16d6657276d57a7e5c4f6c7.

--- src/pages/blog/BlogPage.tsx
+++ src/pages/blog/BlogPage.tsx
@@ -1,7 +1,12 @@
 import { Box } from '@mui/material';
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Loading } from '../../Loading';
 import styles from './BlogPage.module.scss';
 
 export function BlogPage({ ...props }: { children?: React.ReactNode }) {
-  return <Box className={styles.container}>{props.children}</Box>;
+  return (
+    <Suspense fallback={<Loading />}>
+      <Box className={styles.container}>{props.children}</Box>
+    </Suspense>
+  );
 }
--- src/pages/overview/OverviewPage.tsx
+++ src/pages/overview/OverviewPage.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+import { Loading } from '../../Loading';
 import { ARVPSection } from './ARVPSection';
 import { ContactForm } from './ContactForm';
 import { ExperienceSection } from './ExperienceSection';
@@ -8,13 +10,15 @@ import { ReadingListSection } from './ReadingListSection';
 
 export default function OverviewPage() {
   return (
-    <div className={styles.container}>
-      <HeroSection />
-      <ARVPSection />
-      <ExperienceSection />
-      <ProjectsSection />
-      <ContactForm />
-      <ReadingListSection />
-    </div>
+    <Suspense fallback={<Loading />}>
+      <div className={styles.container}>
+        <HeroSection />
+        <ARVPSection />
+        <ExperienceSection />
+        <ProjectsSection />
+        <ContactForm />
+        <ReadingListSection />
+      </div>
+    </Suspense>
   );
 }
